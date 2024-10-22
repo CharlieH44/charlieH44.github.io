553440fe4a0e0da687cd1ae643a1567f923506fd
CharlieH44 <charles.harrison28@student.tamdistrict.org>
2024-10-22 15:09:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let valueOne = 0
 slotValueOne.innerText = valueOne
 function spin() {
-  valueOne = Math.floor(Math.random(*10))
+  valueOne = Math.floor(Math.random()*10)
   slotValueOne.innerText = valueOne
 }
 
